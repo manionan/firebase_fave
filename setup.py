31f2487ce19a3e53dbc0d09d200df5b36bc25d7b
manionan <manionan@gmail.com>
2018-12-10 12:58:59 -0500

    install and init files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='firebase_fave',
+      version='0.1',
+      description='Duct tapes user verification and credentialling functionality onto firebase_admin, attempting to keep it simple',
+      author='Andrew Manion',
+      url='https://github.com/manionan/firebase_fave',
+      packages=['firebase_fave'],
+     )
