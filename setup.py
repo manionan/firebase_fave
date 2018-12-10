db414cd9722f3bf8cfbf6ca71f0d6286dfd9c99b
manionan <manionan@gmail.com>
2018-12-10 14:21:35 -0500

    add info to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(name='firebase_fave',
       version='0.1',
       description='Duct tapes user verification and credentialling functionality onto firebase_admin, attempting to keep it simple',
       author='Andrew Manion',
+      author_email='manion.coolgrammer@gmail.com',
       url='https://github.com/manionan/firebase_fave',
+      download_url='https://github.com/manionan/firebase_fave/archive/v0.1.tar.gz',
       packages=['firebase_fave'],
+      keywords=['firebase', 'firebase_admin']
      )
