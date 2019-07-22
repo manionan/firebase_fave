5856bbb227701f565340ba76d0bb10a273d1d34b
manionan <manionan@gmail.com>
2019-07-22 16:01:48 -0400

    versioning stuff

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='firebase_fave',
-      version='0.2.0',
+      version='0.2.1',
       description='Duct tapes user verification and credentialling functionality onto firebase_admin, attempting to keep it simple',
       author='Andrew Manion',
       author_email='manion.coolgrammer@gmail.com',
       url='https://github.com/manionan/firebase_fave',
-      download_url='https://github.com/manionan/firebase_fave/archive/v0.2.0.tar.gz',
+      download_url='https://github.com/manionan/firebase_fave/archive/v0.2.1.tar.gz',
       packages=['firebase_fave'],
       keywords=['firebase', 'firebase_admin']
      )
